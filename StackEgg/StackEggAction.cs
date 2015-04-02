7b367857a65582d5c4f3e4ee370f86efe6b53f09
Benjamin Dumke-von der Ehe <ben@stackoverflow.com>
2015-04-02 13:37:09 +0200

    StackEgg core game logic, and a minimal console app to play it

---
+++ StackEgg/StackEggAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackEgg
+{
+    public enum StackEggAction
+    {
+        Nothing = 0,
+
+        // the follwing five are the "base actions"
+        Ask,
+        Answer,
+        Upvote,
+        Downvote,
+        Close,
+
+        FlagForModerator
+    }
+}
---
+++ StackEgg/StackEggStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackEgg
+{
+    public enum StackEggStat
+    {
+        Questions = 1,
+        Answers,
+        Users,
+        Quality,
+        Traffic
+    }
+}
